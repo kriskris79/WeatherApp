8e5b75e70b82569c3ae42e18cbe89bee93c7ba13
kriskris79 <krzysztofkakolewski@googlemail.com>
2024-03-11 21:43:19 +0000

    api call

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Weather from './components/Weather';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <main>
+                <Weather />
+            </main>
+        </div>
+    );
+}
+
+export default App;

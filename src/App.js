6267116d7fa899cf2dac6ed92e5524291d5e5124
kriskris79 <krzysztofkakolewski@googlemail.com>
2024-03-11 14:26:56 +0000

    app.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <main>
+                <Weather />
+            </main>
+            <footer>
+            </footer>
+        </div>
+    );
+}
+
+export default App;
